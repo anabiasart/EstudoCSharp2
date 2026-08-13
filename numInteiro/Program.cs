8f87ced6c687c7fb50cd9b27054c47c9c275437e
ana <akirayang727@gmail.com>
2026-08-13 15:55:51 -0300

    dd exerc numinteiro

---
+++ numInteiro/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Numero
+{
+    class Program
+    {
+        static string VerificarNumero(int num)
+        {
+            if (num > 0)
+            {
+                return "Positivo";
+            }
+            else if (num<0)
+            {
+                return "Negativo";
+            }
+            else
+            {
+                return "Zero";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            string resultado = VerificarNumero(num);
+
+            Console.WriteLine($"Resultado: {resultado}");
+        }
+    }
+}
