9429f27c1d75f8071486519e3c4844ee87f0d602
ana <akirayang727@gmail.com>
2026-08-13 15:07:33 -0300

    first

---
+++ proximo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace proximo
+{
+    class Program
+    {
+        static int Numero100(int a, int b)
+        {
+            
+           if(a == b)
+           return 0;
+
+           int distanciaA = Math.Abs(100 - a);
+           int distanciaB = Math.Abs(100 - b);
+
+           if(distanciaA < distanciaB)
+           return a;
+           else
+              return b;
+        
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = Numero100(a, b);
+            Console.WriteLine($"O número mais próximo de 100 é: {resultado}");
+        }
+    }
+}
---
+++ proximo50/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace proximo.cinquenta
+{
+    class Program
+    {
+        static int cinquenta(int a, int b)
+        {
+            if (a==b)
+            return 0;
+
+            int distanciaA = Math.Abs(50 - a);
+            int distanciaB = Math.Abs(50 - b);
+
+            if(distanciaA < distanciaB)
+                return a;
+            else
+                return b;
+
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = cinquenta(a, b);
+            Console.WriteLine($"O número mais próximo de 50 é: {resultado}");
+        }
+    }
+}
