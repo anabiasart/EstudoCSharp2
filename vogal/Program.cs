872c5286c2b3f2294e0fcb3da947756db13cf4b2
ana <akirayang727@gmail.com>
2026-08-17 14:02:00 -0300

    add exerc vogais contain

---
+++ vogal/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite uma palavra ou uma frase: ");
+       string palavra = Console.ReadLine();
+
+string vogais = "aeiou";
+int contador = 0;
+
+for (int i = 0; i < palavra.Length; i++)
+{
+    char letra = palavra[i];
+
+    if (vogais.Contains(letra))
+    {
+        contador++;
+    }
+}
+
+Console.WriteLine("Quantidade: " + contador);
+
+}}
