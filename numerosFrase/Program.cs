1ae2ff7a44431a1e3b9648dae18e262009686b1e
ana <akirayang727@gmail.com>
2026-08-17 18:14:05 -0300

    add exerc numeros na frase

---
+++ numerosFrase/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http.Headers;
+//um programa que pede ao usuario uma frase, e descobre quantos numericos
+//existem nela.
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite uma frase que deseja analisar:");
+        string palavra = Console.ReadLine();
+        
+        int contador =0;
+        
+
+        for (int i = 0; i < palavra.Length; i++)
+        {
+            char caractere = palavra[i];
+
+            if (char.IsDigit(caractere))
+            {
+                contador++;
+            }
+        }
+         Console.WriteLine("quantidade de numeros: "+ contador);
+    }
+}
