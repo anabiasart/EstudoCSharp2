a6ab70f2e9d7d7caf44da9eb76b32dbe99917d3a
ana <akirayang727@gmail.com>
2026-08-17 14:30:58 -0300

    add exerc contador de letra

---
+++ conta/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite uma palvra ou frase: ");
+        string palavra = Console.ReadLine();
+
+        Console.WriteLine("Digite uma letra que deseja procurar: ");
+        char letra = Convert.ToChar(Console.ReadLine());
+        
+        
+        int contador= 0;
+
+        for(int i = 0; i < palavra.Length; i++)
+        {
+            char comp = palavra[i];
+            if (comp == letra)
+            {
+                contador++;
+            }
+        }
+    Console.WriteLine("quantidade: "+ contador);
+    }
+}
