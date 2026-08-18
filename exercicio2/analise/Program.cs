68f8d1e4be566823c35b883a1a739cff9f7fa7f5
ana <akirayang727@gmail.com>
2026-08-18 18:39:41 -0300

    add exerc analise

---
+++ exercicio2/analise/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int positivo =0;
+        int negativo = 0;
+        int zero = 0;
+        double soma = 0;
+        double somaN =0;
+        for(int i =1; i <= 10; i++)
+        {
+            Console.WriteLine("Digite um numero: ");
+            double numero = Convert.ToDouble(Console.ReadLine());
+               
+            if(numero > 0)
+            {
+                positivo++;
+                soma += numero;
+
+            }
+            if (numero < 0)
+            {
+                negativo++;
+                somaN += numero;
+
+            }if(numero == 0)
+            {
+                zero++;
+            }
+
+        }
+
+        Console.WriteLine("Positivo: "+ positivo);
+        Console.WriteLine("Negativo: "+ negativo);
+        Console.WriteLine("SomaPositivo: "+ soma);
+        Console.WriteLine("SomaNeg: "+ somaN);
+        Console.WriteLine("Zeros:"+ zero);
+    }
+}
