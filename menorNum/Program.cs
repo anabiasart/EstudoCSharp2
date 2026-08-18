2dac86481c0f5ca2772b6bab2a02adaea7ac336a
ana <akirayang727@gmail.com>
2026-08-18 14:18:37 -0300

    add exerc menor numero

---
+++ menorNum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int menor = int.MaxValue;
+        for(int i=1; i <= 6; i++)
+        {
+            Console.WriteLine("Digite um numero: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if(numero < menor)
+            {
+                menor=numero;
+            }
+        }
+
+        Console.WriteLine("Menor numero: "+ menor);
+    }
+}
