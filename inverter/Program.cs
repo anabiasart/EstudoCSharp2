2891dbfe197a15ca67104510aa7241852cbe14c8
ana <akirayang727@gmail.com>
2026-08-18 13:22:22 -0300

    add exerc inverter palavra

---
+++ inverter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+//Faça um programa que peça uma 
+// palavra ao usuário e mostre essa palavra ao contrário.
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite uma palavra: ");
+        string palavra = Console.ReadLine();
+        
+        
+        for(int i = palavra.Length - 1; i >=0; i--)
+        {
+               Console.Write(palavra[i]);
+
+        }
+
+
+    }
+}
