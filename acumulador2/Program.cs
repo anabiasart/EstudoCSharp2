9ddac721be5886b0eec90a5a1123ed044ef73404
ana <akirayang727@gmail.com>
2026-08-18 15:31:12 -0300

    add acumulador2

---
+++ acumulador2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        double soma = 0;
+        int contador =0;
+        for(int i =1; i <= 5; i++)
+        {
+            Console.WriteLine("Digite uma nota: ");
+            double numero = Convert.ToDouble(Console.ReadLine());
+                soma += numero;
+           if(numero >= 7)
+            {
+                contador++;
+            }
+           
+            
+ }
+    double media = soma / 5;
+            Console.WriteLine("A media e: "+ media);
+            Console.WriteLine("Notas: "+ contador);
+
+    }
+}
