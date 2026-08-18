839b518da961a6d49b015f7b4e71c27156b83f11
ana <akirayang727@gmail.com>
2026-08-18 13:58:26 -0300

    add exerc maior numero

---
+++ maiorNum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+// mostrar qual o maior numero digitado
+class Program
+{
+    static void Main()
+    {
+        int maior =int.MinValue;
+        for(int i=1; i <= 5; i++)
+        {
+            Console.WriteLine("Digite um numero: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+           if(numero > maior)
+            {
+                maior=numero;
+                
+
+            }
+        }
+
+                Console.WriteLine("Maior número: " + maior);
+
+    }
+}
