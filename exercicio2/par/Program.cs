daa6c6bda2d7a6097584f461c6a6e0e5285f2d31
ana <akirayang727@gmail.com>
2026-08-18 15:56:46 -0300

    add exerc par e impar

---
+++ exercicio2/par/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {   
+        int contadorImpar = 0;
+        int contadorPar=0;
+        int somaPar=0;
+        for(int i = 1; i <= 8; i++)
+        {
+            Console.WriteLine("Digite um numero inteiro: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            
+
+            if(numero % 2 == 0)
+            {   
+
+                contadorPar++;
+                somaPar+=numero;
+            }
+            else
+            {
+                contadorImpar++;
+            }
+
+        }
+
+        Console.WriteLine("Pares: "+ contadorPar);
+        Console.WriteLine("Impares: "+ contadorImpar);
+        Console.WriteLine("Soma dos Pares: "+ somaPar);
+    }
+}
