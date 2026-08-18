020ed007530695a986cb6c2fd1cdd9357f50af0f
ana <akirayang727@gmail.com>
2026-08-18 14:25:17 -0300

    add exer menor numero e o maior

---
+++ maiorEmenor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int menor = int.MaxValue;
+        int maior = int.MinValue;
+        for(int i =1; i<= 5; i++)
+        {
+            Console.WriteLine("Digite um numero: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if(numero > maior)
+            {
+                maior =numero;
+            } if(numero < menor)
+            {
+                menor=numero;
+            }
+        }
+        Console.WriteLine("Maior numero: " + maior);
+        Console.WriteLine("Menor numero: "+ menor);
+    }
+}
