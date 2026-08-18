3194386ab4b6f04127668de3586d80ec34c8c8da
ana <akirayang727@gmail.com>
2026-08-18 14:51:43 -0300

    add exerc media das notas

---
+++ acumulador/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        double media;
+        double soma =0;
+        for(int i =1; i <= 5; i++)
+        {
+            Console.WriteLine("Digite uma nota: ");
+            double numero = Convert.ToDouble(Console.ReadLine());
+         soma += numero;    
+
+                   
+        }
+            
+            
+            media = soma / 5;
+            Console.WriteLine("A media e: "+ media);
+
+        
+    }
+    
+}
