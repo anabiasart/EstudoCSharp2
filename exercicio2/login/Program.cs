0d58bfe336f8276b4720d81bc9e84ac67ad616b4
ana <akirayang727@gmail.com>
2026-08-19 16:41:11 -0300

    add exerc login

---
+++ exercicio2/login/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string usuario = "admin";
+        string senha = "1234";
+        int tentativas = 3;
+
+
+        bool logado = false;
+
+        while (tentativas > 0)
+        {
+            Console.WriteLine("Informe o usuario: ");
+            string user = Console.ReadLine();
+
+            Console.WriteLine("Informe a senha: ");
+            string pass = Console.ReadLine();
+             
+             
+          if (user != usuario || pass != senha)
+            {
+                tentativas--;
+                Console.WriteLine("Usuario ou senha incorretos");
+                Console.WriteLine("Tentativas restantes: "+ tentativas);
+
+                if(tentativas == 0)
+                {
+                Console.WriteLine("Acesso bloqueado.");                    
+                }
+        }
+
+                else
+                {
+                logado = true;
+
+                Console.WriteLine("Login realizado com sucesso");
+                break;
+                }
+            }
+
+
+            if (logado)
+            {
+                Console.WriteLine("[1] Perfil");
+                Console.WriteLine("[2] Alterar senha");
+                Console.WriteLine("[3] Sair");
+                
+            }
+        
+    }
+}
