f073ec5e5a2a355baa4b81ce2cd215e7bed4c0f1
ana <akirayang727@gmail.com>
2026-08-19 14:52:27 -0300

    add exerc cadastro funcionario

---
+++ exercicio2/cadastroF/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] nomes = new string[5];
+        double[] salarios = new double[5];
+        
+
+        for(int i=1; i <= 5; i++)
+        {
+            Console.WriteLine("Digite o nome do funcioanrio: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Salario: ");
+            double salario = Convert.ToDouble(Console.ReadLine());
+           
+           if(salario <= 0)
+            {
+                Console.WriteLine("Salario invalido.Digite novamente");
+                i--;
+                continue;
+            }
+            nomes[i -1] =nome;
+            salarios[i-1]=salario;
+        }
+
+        Console.WriteLine("Digite o nome do funcionario que deseja buscar: ");
+        string procura = Console.ReadLine();
+        bool encontrado = false;
+
+        for (int i = 0; i < nomes.Length; i++)
+{
+            if (nomes[i].Equals(procura, StringComparison.OrdinalIgnoreCase))
+            {
+        encontrado = true;
+        Console.WriteLine("Funcionário encontrado!");
+        Console.WriteLine("Nome: " + nomes[i]);
+        Console.WriteLine("Salário: R$ " + salarios[i]);
+        Console.WriteLine("Posição no cadastro: " + (i + 1));
+        break;
+            }
+}
+          
+            if (encontrado == false)
+            {
+                Console.WriteLine("Nao encontrado");
+            }
+        }
+    }
--- exercicio2/estoque/Program.cs
+++ exercicio2/estoque/Program.cs
@@ -4,11 +4,11 @@ class Program
 {
     static void Main()
     {int[] quantidades = new int[6];
-    string[] produtos = new string[6];
+     string[] produtos = new string[6];
         int maior=int.MinValue;
         int menor=int.MaxValue;
-      string nomeMaior = "";
-string nomeMenor = "";
+          string nomeMaior = "";
+            string nomeMenor = "";
         int total=0;
         int menor5=0;
         for(int i=1; i <= 6; i++)
@@ -23,8 +23,9 @@ string nomeMenor = "";
     Console.WriteLine("Quantidade inválida. Digite novamente.");
     i--;
     continue;
-}produtos[i - 1] = produto;
-quantidades[i - 1] = quantidade;
+}
+            produtos[i - 1] = produto;
+                quantidades[i - 1] = quantidade;
             if (quantidade > maior)
             {   
                 maior=quantidade;
@@ -46,17 +47,17 @@ quantidades[i - 1] = quantidade;
 
         }
 
-       Console.WriteLine("Total de itens: " + total);
+            Console.WriteLine("Total de itens: " + total);
 
-Console.WriteLine("Maior estoque: " + nomeMaior);
-Console.WriteLine("Quantidade: " + maior);
+            Console.WriteLine("Maior estoque: " + nomeMaior);
+            Console.WriteLine("Quantidade: " + maior);
 
-Console.WriteLine("Menor estoque: " + nomeMenor);
-Console.WriteLine("Quantidade: " + menor);
+            Console.WriteLine("Menor estoque: " + nomeMenor);
+            Console.WriteLine("Quantidade: " + menor);
 
-Console.WriteLine("Produtos com estoque baixo: " + menor5);
+            Console.WriteLine("Produtos com estoque baixo: " + menor5);
 
-Console.WriteLine("\nProdutos que precisam de reposição:");
+            Console.WriteLine("\nProdutos que precisam de reposição:");
 
 for (int i = 0; i < produtos.Length; i++)
 {
