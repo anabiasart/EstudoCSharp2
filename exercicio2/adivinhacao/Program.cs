30c96c959db14162968b6a70784063b122076341
ana <akirayang727@gmail.com>
2026-08-20 17:01:30 -0300

    add exerc troco

---
+++ exercicio2/adivinhacao/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int numeroSecreto =random.Next(1,101);
+        int tentativas=0;
+        bool acertou=false;
+
+      
+
+    while(!acertou)
+    {
+         Console.WriteLine("Digite seu palpite: ");
+        int palpite = Convert.ToInt32(Console.ReadLine());
+    
+    if (palpite < 1 || palpite > 100)
+    {
+        Console.WriteLine("Palpite invalido.");
+        continue;
+    }
+
+    tentativas++;
+
+        if(palpite < numeroSecreto)
+        {
+            Console.WriteLine("O numero é maior");
+
+        }else if(palpite > numeroSecreto)
+        {
+            Console.WriteLine("Numero é menor");
+
+        }
+       else
+            {
+                acertou = true;
+                tentativas++;
+
+                Console.WriteLine("Voce acertou!");
+                Console.WriteLine("Tentativas: " + tentativas);
+            }
+    
+}
+}
+}
+       
---
+++ exercicio2/troco/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool executando =true;
+    while(executando){
+
+        int opcao=0;
+
+        Console.WriteLine("Valor da compra: ");
+        double valor = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Valor recebido: ");
+        double recebido  = Convert.ToDouble(Console.ReadLine());
+        
+        if(valor <= 0)
+        {
+            Console.WriteLine("Valor da compra invalido!");
+            continue;
+        }if(recebido < valor)
+        {  
+            Console.WriteLine("Valor insuficiente!");
+            double faltare = valor-recebido;
+            Console.WriteLine("Faltam: "+faltare);
+        }else if (recebido == valor)
+        {
+            Console.WriteLine("Pagamento exato, nao tem troco");
+        }
+        else
+        {
+            double troco = recebido-valor;
+            Console.WriteLine("Troco: "+ troco);
+        }
+        Console.WriteLine("Deseja realizar outra compra? ");
+        Console.WriteLine("[1] Sim");
+        Console.WriteLine("[2] Não");
+        opcao = Convert.ToInt32(Console.ReadLine());
+            if (opcao == 1)
+            {
+               continue;
+            }else if(opcao == 2)
+            {
+                executando = false;
+
+                Console.WriteLine("Caixa encerrado");
+                
+            }
+            else
+            {
+                Console.WriteLine("Opcao invalida, digite novamente");
+                continue;
+            }
+        
+    }
+    }
+}
