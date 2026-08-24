6b7a5378bee7ecb2a80085b496a080a1db71f250
ana <akirayang727@gmail.com>
2026-08-24 15:42:16 -0300

    add att

---
+++ exercicio2/detectorSe/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    { 
+        int anterior = 0;
+        int maiorAumento=0;
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Digite um numero inteiro: ");
+                int numero = Convert.ToInt32(Console.ReadLine());
+
+                if (i > 1)
+                {
+                    if (numero > anterior)
+                    {
+                        int aumento = numero - anterior;
+
+                        if (aumento > maiorAumento)
+                        {
+                            maiorAumento = aumento;
+                        }
+                    }
+                }
+
+                anterior = numero;
+            }
+            Console.WriteLine("Maior aumento: " + maiorAumento);
+    }
+}
