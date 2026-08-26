5837a6ec270f492ae3b2742553d773f882b96a01
ana <akirayang727@gmail.com>
2026-08-26 16:45:34 -0300

    att

---
+++ exercicio2/pocoes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] nomes= new string[5];
+        int[] poder = new int[5];
+        int[] estoque = new int[5];
+        
+        bool pode = true;
+
+        while (pode)
+        {
+            Console.WriteLine("=====LAB====");
+            Console.WriteLine("[1] Cadastrar poções");
+            Console.WriteLine("[2] Listar Poções ");
+            Console.WriteLine("[3] Usar Poção");
+            Console.WriteLine("[4] Repor Estoque");
+            Console.WriteLine("[5] Buscar poção");
+            Console.WriteLine("[6] Sair");
+
+        int opcao = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+    }
+
+    static void ListarPocoes(string[] nomes, int[] poder, int[] estoque)
+    {
+        
+        return ListarPocoes;
+    }
+
+    static void CadastrarPocoes(string[] nomes, int[] poder, int[] estoque)
+{
+}
+
+static int BuscarPocao(string[] nomes, string nomeBuscado)
+{
+    // retorna o índice
+}
+
+
+}
